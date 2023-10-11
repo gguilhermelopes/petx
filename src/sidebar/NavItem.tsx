930eaecdc65dd565e16aba9ae7678e3a106c3411
gguilhermelopes <guilhermelopes275@gmail.com>
2023-10-11 14:34:03 -0300

    feat: add style to current location in navlink

--- src/sidebar/NavItem.tsx
+++ src/sidebar/NavItem.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 type NavItemProps = {
   label: string;
@@ -7,9 +7,14 @@ type NavItemProps = {
 };
 
 const NavItem = ({ label, icon, path }: NavItemProps) => {
+  const location = useLocation();
+  const isCurrentLocation = location.pathname === path;
   return (
     <NavLink
       to={path}
+      style={{
+        backgroundColor: isCurrentLocation ? "#ffffff" : "transparent",
+      }}
       className="flex items-center gap-4 px-3 py-2 text-[1.75rem] text-p3 font-medium cursor-pointer transition-all rounded-2xl hover:bg-c0"
     >
       <img src={icon} />
